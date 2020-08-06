faabce25f8035303ca9c06eccf82f3adfeb80095
ace-tronaut <69324946+ace-tronaut@users.noreply.github.com>
2020-08-07 00:49:43 +0400

    Add files via upload

---
+++ morseL.py
@@ -0,0 +1,56 @@
+import datetime
+import csv
+import os
+#import mysql.connector
+import sys
+import time
+#print("WELCOME TO morseL!")
+print(
+    "                                                   ███╗   ███╗ ██████╗ ██████╗ ███████╗███████╗██╗     "
+)
+print(
+    "                                                   ████╗ ████║██╔═══██╗██╔══██╗██╔════╝██╔════╝██║     "
+)
+print(
+    "                                                   ██╔████╔██║██║   ██║██████╔╝███████╗█████╗  ██║     "
+)
+print(
+    "                                                   ██║╚██╔╝██║██║   ██║██╔══██╗╚════██║██╔══╝  ██║     "
+)
+print(
+    "                                                   ██║ ╚═╝ ██║╚██████╔╝██║  ██║███████║███████╗███████╗"
+)
+print(
+    "                                                   ╚═╝     ╚═╝ ╚═════╝ ╚═╝  ╚═╝╚══════╝╚══════╝╚══════╝"
+)
+print()
+e = ("The best Morse code trainer!")
+for i in e:
+    sys.stdout.write(i)
+    sys.stdout.flush()
+    time.sleep(0.04)
+# Will redo the heading with ASCII art for "pazzaz" and effect
+print()
+time.sleep(1)
+print(180 * "-")
+time.sleep(2)
+Mrse = (
+    "Morse code; developed by Samuel Morse is an intricately designed system of letters which can be communicated through dots and dashes hence it can be delivered through a variety of mediums adding to it's versatility and usefulness."
+)
+for i in Mrse:
+    sys.stdout.write(i)
+    sys.stdout.flush()
+    time.sleep(0.04)
+time.sleep(2)
+print()
+print()
+mrs = (
+    "In this program you will be able to learn how to use Morse code through training modules and games or if you want you can input any regular text and convert it into Morse code and vice-versa"
+)
+for i in mrs:
+    sys.stdout.write(i)
+    sys.stdout.flush()
+    time.sleep(0.04)
+print()
+print()
+wer = ("Now let's go!")

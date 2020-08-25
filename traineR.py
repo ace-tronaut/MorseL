43b682d1c3a54da74e2c0888ae0d3ee1177e1d4b
ace-tronaut <69324946+ace-tronaut@users.noreply.github.com>
2020-08-26 01:31:58 +0400

    Update traineR.py

--- traineR.py
+++ traineR.py
@@ -19,7 +19,64 @@ for i in qool:
     sys.stdout.write(i)
     sys.stdout.flush()
     time.sleep(0.04)
+    
+print()    
 
 for i in range(26):
-    rand = random.randint(26)
-    print(morseBase[rand[1]])
+    print(morseBase[i[1]], end = "")
+    print(morseBase[i[2]])
+    ans = input('Enter in the dots you see; use periods and hyphens: ')
+    print()
+    if ans == morseBase[i[2]]:
+        j = random.randint(3)
+        print(Congo[j])
+        print()
+    else:
+        j = random.randint(3)
+        print(nocongo[j])
+        print()
+        while a!=0:
+            anss = input('Try again: ')
+            if anss = morseBase[i[2]]:
+                j = random.randint(3)
+                print(Congo[j])
+                print()
+                a=0
+            else:
+                j = random.randint(3)
+                print(nocongo[j])
+                print()
+
+hool = (
+    "Now we'll show you the Morse and then it's corresponding letter, you have to type them out; Easy as that?"
+)
+for i in hool:
+    sys.stdout.write(i)
+    sys.stdout.flush()
+    time.sleep(0.04)
+
+for i in range(26):
+    print(morseBase[i[1]], end = "")
+    print(morseBase[i[2]])
+    ans = input('Enter in the letter of the Morse: ')
+    print()
+    if ans == morseBase[i[0]]:
+        j = random.randint(3)
+        print(Congo[j])
+        print()
+    else:
+        j = random.randint(3)
+        print(nocongo[j])
+        print()
+        a=2
+        while a!=0:
+            anss = input('Try again: ')
+            if anss = morseBase[i[0]]:
+                j = random.randint(3)
+                print(Congo[j])
+                print()
+                a=0
+            else:
+                j = random.randint(3)
+                print(nocongo[j])
+                print()
